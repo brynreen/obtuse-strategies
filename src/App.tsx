fabc7dfb917327523d7b36d30770fbe7427d5906
brynreen <45588892+brynreen@users.noreply.github.com>
2024-06-06 17:51:40 -0400

    css reset; add random button

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,21 @@
 import { strategies } from './strategies.json';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
-  const index = Math.floor(Math.random() * strategies.length);
-  const strategy = strategies[index];
+  const [index, setIndex] = useState(Math.floor(Math.random() * strategies.length));
+
+  function handleClick() {
+    setIndex(Math.floor(Math.random() * strategies.length));
+  }
+
   return (
     <>
       <h1>Obtuse Strategies</h1>
-      <p>{strategy}</p>
+      <p className="strategy">{strategies[index]}</p>
+      <button onClick={handleClick}>
+        Random
+      </button>
     </>
   );
 }
